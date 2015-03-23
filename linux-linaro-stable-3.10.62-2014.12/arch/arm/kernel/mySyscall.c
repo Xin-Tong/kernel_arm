aefe337bcc7b0e5e586f8d521d9c1da21557861b
Xin-Tong <tongxinhit@gmail.com>
2015-03-22 21:28:43 -0400

    add new syscall

---
+++ linux-linaro-stable-3.10.62-2014.12/arch/arm/kernel/mySyscall.c
@@ -0,0 +1,39 @@
+
+/*
+ *  linux/arch/arm/kernel/sys_arm.c
+ *
+ *  Copyright (C) People who wrote linux/arch/i386/kernel/sys_i386.c
+ *  Copyright (C) 1995, 1996 Russell King.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ *  This file contains various random system calls that
+ *  have a non-standard calling sequence on the Linux/arm
+ *  platform.
+ */
+#include <linux/export.h>
+#include <linux/errno.h>
+#include <linux/sched.h>
+#include <linux/mm.h>
+#include <linux/sem.h>
+#include <linux/msg.h>
+#include <linux/shm.h>
+#include <linux/stat.h>
+#include <linux/syscalls.h>
+#include <linux/mman.h>
+#include <linux/fs.h>
+#include <linux/file.h>
+#include <linux/ipc.h>
+#include <linux/uaccess.h>
+#include <linux/slab.h>
+
+
+/* added without*/
+SYSCALL_DEFINE2(sched_setlimit, int, pid, int, limit)
+{
+    printk(KERN_EMERG "My syscall works! The pid is %d, and the limit is %d.\n",
+           (int)pid, limit);
+    return pid+limit;
+} 
